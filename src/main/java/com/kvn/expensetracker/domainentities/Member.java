a964190bf541c1935f053b9a7d402135b7ac5412
pkoppula <pkoppula@sapient.com>
2018-07-29 17:13:25 +0530

    refinement

--- src/main/java/com/kvn/expensetracker/domainentities/Member.java
+++ src/main/java/com/kvn/expensetracker/domainentities/Member.java
@@ -3,6 +3,7 @@ package com.kvn.expensetracker.domainentities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 // TODO: Auto-generated Javadoc
@@ -14,7 +15,7 @@ public class Member {
 
 	/** The id. */
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	public int id;
 	
 	/** The first name. */
