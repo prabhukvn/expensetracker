ca84902a50f305b708055c2dad6811c9731ab773
prabhukvn <prabhukvn@gmail.com>
2019-11-02 11:38:52 +0530

    changes

--- src/main/java/com/kvn/expensetracker/controllers/EventsController.java
+++ src/main/java/com/kvn/expensetracker/controllers/EventsController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -50,6 +51,7 @@ public class EventsController {
 	 * @param event the event
 	 * @return the response entity
 	 */
+
 	@EnablePerformanceLogging
 	@PostMapping(value = "/events", produces = "application/json")
 	public ResponseEntity<Event> createEvent(@RequestBody Event event) {
